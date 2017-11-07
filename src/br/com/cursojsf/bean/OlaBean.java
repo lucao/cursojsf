7a26c963046f255528971f26577d3bac631a9bcc
lucas.mteixeira <lucas.mteixeira@RJOD230145>
2017-11-07 12:06:30 -0200

    alterações de legibilidade

--- src/br/com/cursojsf/bean/OlaBean.java
+++ src/br/com/cursojsf/bean/OlaBean.java
@@ -14,6 +14,7 @@ public class OlaBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String message;
+	
 
 	public String getMessage() {
 		return message;
