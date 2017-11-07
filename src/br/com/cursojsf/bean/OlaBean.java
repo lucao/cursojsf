4eecc9bea30e84dc3219cd3640991e9a9e4dd3a9
lucas.mteixeira <lucas.mteixeira@RJOD230145>
2017-11-07 11:44:58 -0200

    pequenas alterações

--- src/br/com/cursojsf/bean/OlaBean.java
+++ src/br/com/cursojsf/bean/OlaBean.java
@@ -1,11 +1,18 @@
 package br.com.cursojsf.bean;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class OlaBean {
+public class OlaBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private String message;
 
 	public String getMessage() {
